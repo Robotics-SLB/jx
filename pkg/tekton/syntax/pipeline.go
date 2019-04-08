54f2451bbf4e2458a447f4660793c1fee30e1d62
rawlingsj <rawlingsj80@gmail.com>
2019-04-08 18:03:33 +0100

    fix pipeline syntax issue where entrypoint was failing to run command and args as a string

--- pkg/tekton/syntax/pipeline.go
+++ pkg/tekton/syntax/pipeline.go
@@ -995,10 +995,12 @@ func generateSteps(step Step, inheritedAgent string, env []corev1.EnvVar, parent
 			c.Image = stepImage
 			c.Command = []string{"/bin/sh", "-c"}
 		}
-		c.Args = []string{step.Command}
+		foo := step.Command
 		if len(step.Arguments) > 0 {
-			c.Args = append(c.Args, strings.Join(step.Arguments, " "))
+			foo += " " + strings.Join(step.Arguments, " ")
 		}
+		c.Args = []string{foo}
+
 		c.WorkingDir = workingDir
 		stepCounter++
 		if step.Name != "" {
